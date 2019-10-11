958c9dae3c650022c1340ff8e9102ad82a0afa91
Eran Stiller <git@stiller.blog>
2019-10-11 16:06:41 +0300

    Setup AKS & ACR

---
+++ k8s/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# az login
+az group create --name dotnetconfdemo --location westeurope
+az acr create --resource-group dotnetconfdemo --name dotnetconfdemo --sku Basic
+az aks create --resource-group dotnetconfdemo --name dotnetconfdemo --node-count 1 --enable-addons monitoring --generate-ssh-keys
+az aks install-cli
+az aks get-credentials --resource-group dotnetconfdemo --name dotnetconfdemo
+kubectl get nodes
